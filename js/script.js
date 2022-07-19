bb994a09cbb7b4feafd058068ed8c59a210a2d30
mullenslucas <mullenlucas@gmail.com>
2022-07-19 13:51:00 -0300

    fix linter errors in javascript ESlint

--- js/script.js
+++ js/script.js
@@ -1,55 +1,56 @@
-const hambIcon = document.getElementById("hamburgerIcon");
-const closeIcon = document.getElementById("closeIcon");
-const menuItems = document.getElementById("myMenuItems")
+const hambIcon = document.getElementById('hamburgerIcon');
+const closeIcon = document.getElementById('closeIcon');
+const menuItems = document.getElementById('myMenuItems');
 
 // Operation when SHOWING items
 const showItems = function () {
-  hambIcon.style.display = "none";
-  hambIcon.style.visibility = "hidden";
-  closeIcon.style.display = "block";
-  closeIcon.style.visibility = "visible";
-  menuItems.style.display = "flex";
-  menuItems.style.visibility = "visible";
-}
+  hambIcon.style.display = 'none';
+  hambIcon.style.visibility = 'hidden';
+  closeIcon.style.display = 'block';
+  closeIcon.style.visibility = 'visible';
+  menuItems.style.display = 'flex';
+  menuItems.style.visibility = 'visible';
+};
 // Operation when HIDING items
 const hideItems = function () {
-  closeIcon.style.display = "none";
-  closeIcon.style.visibility = "hidden";
-  hambIcon.style.display = "block";
-  hambIcon.style.visibility = "visible";
-  menuItems.style.display = "none";
-  menuItems.style.visibility = "hidden";
-}
+  closeIcon.style.display = 'none';
+  closeIcon.style.visibility = 'hidden';
+  hambIcon.style.display = 'block';
+  hambIcon.style.visibility = 'visible';
+  menuItems.style.display = 'none';
+  menuItems.style.visibility = 'hidden';
+};
 // Handling hamburgerMenu open/close behavior
 const hamburgerMenu = function () {
-  if (hambIcon.style.display === "block") {
-    showItems()
+  if (hambIcon.style.display === 'block') {
+    showItems();
   } else {
-    hideItems()
+    hideItems();
   }
-}
+};
 // Handling hamburgerMenu open/close behavior when clicking item
-const adaptHamburgerMenu = function (){
-  const menuItems = document.getElementById("myMenuItems")
-  if (menuItems.style.display === "flex") {
-    hideItems()
+const adaptHamburgerMenu = function () {
+  const menuItems = document.getElementById('myMenuItems');
+  if (menuItems.style.display === 'flex') {
+    hideItems();
   }
-}
-/* When changing screen-size after closing the hamburger menu (therefore, hiding the "menuItems" list)
-the items remain hidden on desktop. 
+};
+/* When changing screen-size after closing the hamburger menu 
+(therefore, hiding the "menuItems" list)
+so the items remain hidden on desktop. 
 This shouldn't really be a problem since users wouldn't be browsing from one screen size to another,
 in the same session,
 but just to make the code function properly in such unrealistic scenarios, this fix is added */
 
-const x = window.matchMedia("(min-width: 768px)")
+const x = window.matchMedia('(min-width: 768px)');
 
 x.onchange = (e) => {
-  const menuItems = document.getElementById("myMenuItems")
+  const menuItems = document.getElementById('myMenuItems');
   if (e.matches) {
-    menuItems.style.display = "flex";
-    menuItems.style.visibility = "visible";
+    menuItems.style.display = 'flex';
+    menuItems.style.visibility = 'visible';
   } else {
-    menuItems.style.display = "none";
-    menuItems.style.visibility = "hidden";
+    menuItems.style.display = 'none';
+    menuItems.style.visibility = 'hidden';
   }
-}
+};
